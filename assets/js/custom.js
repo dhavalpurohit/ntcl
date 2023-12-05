898076c7defd26a745eea1edcaaaf1ae820df529
Dhaval Purohit <dhavalpurohit.work@gmail.com>
2023-12-05 19:41:42 +0530

    feat: ui design

---
+++ assets/js/custom.js
@@ -0,0 +1,53 @@
+$(function () {
+    // side nav active
+    $("aside li").click(function () {
+        $(this).parent().addClass('active').siblings().removeClass('active')
+
+    })
+
+    // table checked box
+    function setCheck(o, c) {
+        o.prop("checked", c)
+        if (c) {
+            o.closest("tr").addClass("checked")
+        } else {
+            o.closest("tr").removeClass("checked")
+        }
+    }
+
+    function setCheckAll(o, c) {
+        o.each(function () {
+            setCheck($(this), c)
+        })
+        if (c) {
+            $(".check_all").prop("title", "Check All")
+        } else {
+            $(".check_all").prop("title", "Uncheck All")
+        }
+    }
+
+    $(".check_all").on('change', function () {
+        setCheckAll($("tbody input[type='checkbox']"), $(this).prop("checked"))
+    })
+    $("tbody tr").on("click", function (e) {
+        var chk = $("[type='checkbox']", this)
+        setCheck(chk, !$(this).hasClass("checked"))
+    })
+
+
+    // aside responsive
+    $("#open").click(function () {
+        $("aside").addClass("open")
+    })
+    $("#close").click(function () {
+        $("aside").removeClass("open")
+    })
+
+    // profile responsive
+    $("#profile").click(function () {
+        $(".profile").addClass("open")
+    })
+    $("#closeprofile").click(function () {
+        $(".profile").removeClass("open")
+    })
+})
---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,47 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                white: {
+                    default: '#fff',
+                    antiflash: '#F0F3F4',
+                    dutch: '#FBE7C1',
+                    antiflash2: '#F0F3F4'
+                },
+                blue: {
+                    crayola: '#417CFF',
+                    yinmn: '#3F4F72',
+                },
+                silver: '#aaa',
+                green: {
+                    mlachite: '#25D366'
+                },
+                gray: {
+                    davys: '#484848',
+                    platinum: '#EAEAEA'
+                },
+                cyan: {
+                    dark: '#128C7E'
+                },
+                yellow: {
+                    sunglow: '#FDCC54'
+                },
+                red: {
+                    folly: '#F45158'
+                },
+                orange: {
+                    web: '#FFA700'
+                },
+                purple: {
+                    pale: '#F0E6FE'
+                }
+            },
+            borderRadius: {
+                '10px': '0.625rem'
+            },
+            boxShadow: {
+                'lightgray': '0px 2px 8px 0px rgba(99, 99, 99, 0.20);'
+            }
+        }
+    }
+}
